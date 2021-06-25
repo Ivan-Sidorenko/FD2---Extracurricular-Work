9d937c1a5d155c5e8b0e8f16a588a0960ecf3b65
ivan sidorenko <ivan.a.sidorenko@gmail.com>
2021-06-25 16:34:56 +0300

    init: start lern php/mysql/javascript

---
+++ 08_PHP_MySQL_JavaScript_HTML_CSS/index.php
@@ -0,0 +1,57 @@
+<?php //index.php
+    /* $surname = "Fred Smith";
+    echo $surname;
+    echo "<br>";
+    $curent_user = $surname;
+    echo $curent_user; */
+
+    /* $team = array ("Bull", "Mary", "Mike", "Chris", "Anne");
+    echo $team[3]; */
+
+    /* $xox = array(array("x", " ", "0"),
+                 array("0", "0", "x"),
+                 array("x", "0", " "),
+    );
+    echo $xox[1][2]; */
+
+    /* echo 6 + 2; */
+
+    /* $count = 5;
+    echo $count += 1; */
+
+    /* $hour = 13;
+    if ($hour > 12 && $hour < 14) echo "dolunch"; */
+
+    /* $x = 9;
+    if (++$x == 10) echo $x; */
+
+    /* $y = 0;
+    if ($y-- == 0) echo $y; */
+
+    /* $msgs = 5;
+    echo "У вас " . $msgs . " сообщений."; */
+
+    /* $a = "Пере";
+    $b = "крест";
+    $c = "ок";
+    echo $a .= $b .= $c; */
+
+    /* $info = 'Предваряйте имена символов $, как в данном примере: $variable';
+    echo $info; */
+
+    /* $count = 10;
+    echo "На этой неделе ваш профиль просмотрело $count человек"; */
+
+    /* $heading = "Дата\tИмя\tПлатеж";
+    echo $heading; */
+
+    /* $author = "Steve Ballmer";
+    echo "Developers, Developers, developers, developers,
+    developers, developers, developers, developers, developers!
+    $author."; */
+
+    /* $author = "Bill Gates";
+    $text = "Measuring programming progress by lines of code is like
+    measuring aircraft building progress by weight.
+    $author.";
+    echo $text; */
