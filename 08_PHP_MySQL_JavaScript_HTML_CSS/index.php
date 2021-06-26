bd01f2cec26f5efc540c0dc266093aa912b85689
ivan sidorenko <ivan.a.sidorenko@gmail.com>
2021-06-26 21:07:51 +0300

    feat: apdate php/mysql/javascript

--- 08_PHP_MySQL_JavaScript_HTML_CSS/index.php
+++ 08_PHP_MySQL_JavaScript_HTML_CSS/index.php
@@ -1,4 +1,7 @@
 <?php //index.php
+    //Глава 1 примеров не имела
+    //Глава 2 примеров не имела
+    //Глава 3
     /* $surname = "Fred Smith";
     echo $surname;
     echo "<br>";
@@ -55,3 +58,84 @@
     measuring aircraft building progress by weight.
     $author.";
     echo $text; */
+
+    /* $author = "Brian W. Kernigan";
+
+    echo <<<_END
+    Debugging is twice as hard as writing the code in the first place.
+    Therefore, if you write the code as cleaverly as possible, you are, 
+    by definition, not smart enough to debug it.
+    $author.
+    _END; */
+
+    /* $author = "Scott Adams";
+
+    $out = <<<_END
+    Normal people belive that if it ain't broke, don't fix it.
+    Engeneers belive that if it ain't broke, it doesn't have enough
+    reatures yet.
+    - $author.
+    _END;
+    echo $out; */
+
+    /* $number = 12345 * 67890;
+    echo substr($number, 3, 1); */
+
+    /* $pi = "3.1415927";
+    $radius = 5;
+    echo $pi * ($radius * $radius); */
+
+    //Глава 4
+    /* echo "a: [" . true . "]<br>";
+    echo "b: [" . false . "]<br>"; */
+
+    /* echo "a: [" . (20 > 9) . "]<br>";
+    echo "b: [" . (5 == 6) . "]<br>";
+    echo "c: [" . (1 == 0) . "]<br>";
+    echo "d: [" . (1 == 1) . "]<br>"; */
+
+    /* $myname = "Brian";
+    $myage  = 37;
+
+    echo "a: " . 73      . "<br>"; // числовой литерал
+    echo "b: " . "Hello" . "<br>"; // строковый литерал
+    echo "c: " . FALSE   . "<br>"; // константный (резервированное значение) литерал
+    echo "d: " . $myname . "<br>"; // строковая переменная
+    echo "e: " . $myage  . "<br>"; // числовая переменная  */
+
+    /* $day_number = 340;                
+    $days_to_new_year = 366 - $day_number; // выражение
+
+    if ($days_to_new_year < 30)             
+    {
+        echo "Скоро Новый Год!"; // инструкция
+    } */
+
+    /* $month = "March";
+
+    if ($month == "March") echo "Скоро весна!"; */
+
+    /* $a = "1000";
+    $b = "+1000";
+
+    if ($a == $b)  echo "1";
+    if ($a === $b) echo "2"; */
+
+    /* $a = "1000";
+    $b = "+1000";
+
+    if ($a != $b)  echo "1";
+    if ($a !== $b) echo "2"; */
+
+    /* $a = 2; $b = 3;
+    if ($a > $b)  echo "$a больше $b<br>";
+    if ($a < $b)  echo "$a меньше $b<br>";
+    if ($a >= $b) echo "$a больше или равно $b<br>";
+    if ($a <= $b) echo "$a меньше или равно $b<br>"; */
+
+    /* $a = 1; $b = 0;
+
+    echo ($a AND $b) . "<br>";
+    echo ($a or $b)  . "<br>";
+    echo ($a XOR $b) . "<br>";
+    echo !$a . "<br>"; */
